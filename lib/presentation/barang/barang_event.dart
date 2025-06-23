742d230cd3fa814dcada1b785d16d812f3b33c23
Ramanda Dio <ramandadiio@gmail.com>
2025-06-23 22:43:15 +0700

    barang event

---
+++ lib/presentation/barang/barang_event.dart
@@ -0,0 +1,48 @@
+// lib/bloc/barang/barang_event.dart
+import 'package:equatable/equatable.dart';
+import '../../data/models/barang_model.dart';
+
+abstract class BarangEvent extends Equatable {
+  const BarangEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadBarang extends BarangEvent {}
+
+class AddBarang extends BarangEvent {
+  final BarangModel barang;
+
+  const AddBarang(this.barang);
+
+  @override
+  List<Object> get props => [barang];
+}
+
+class UpdateBarang extends BarangEvent {
+  final BarangModel barang;
+
+  const UpdateBarang(this.barang);
+
+  @override
+  List<Object> get props => [barang];
+}
+
+class DeleteBarang extends BarangEvent {
+  final int id;
+
+  const DeleteBarang(this.id);
+
+  @override
+  List<Object> get props => [id];
+}
+
+class DeleteMultipleBarang extends BarangEvent {
+  final List<int> ids;
+
+  const DeleteMultipleBarang(this.ids);
+
+  @override
+  List<Object> get props => [ids];
+}
