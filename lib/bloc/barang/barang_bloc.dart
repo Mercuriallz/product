178269122d76fa79de39b2e636f208332ac80f9d
Ramanda Dio <ramandadiio@gmail.com>
2025-06-24 10:24:31 +0700

    rewrite variables

--- lib/bloc/barang/barang_bloc.dart
+++ lib/bloc/barang/barang_bloc.dart
@@ -11,14 +11,14 @@ class BarangBloc extends Bloc<BarangEvent, BarangState> {
   final BarangRepository barangRepository;
 
   BarangBloc({required this.barangRepository}) : super(BarangInitial()) {
-    on<LoadBarang>(_onLoadBarang);
-    on<AddBarang>(_onAddBarang);
-    on<UpdateBarang>(_onUpdateBarang);
-    on<DeleteBarang>(_onDeleteBarang);
-    on<DeleteMultipleBarang>(_onDeleteMultipleBarang);
+    on<LoadBarang>(onLoadBarang);
+    on<AddBarang>(onAddBarang);
+    on<UpdateBarang>(onUpdateBarang);
+    on<DeleteBarang>(onDeleteBarang);
+    on<DeleteMultipleBarang>(onDeleteMultipleBarang);
   }
 
-  Future<void> _onLoadBarang(LoadBarang event, Emitter<BarangState> emit) async {
+  Future<void> onLoadBarang(LoadBarang event, Emitter<BarangState> emit) async {
     emit(BarangLoading());
     try {
       final barangList = await barangRepository.getAllBarang();
@@ -28,7 +28,7 @@ class BarangBloc extends Bloc<BarangEvent, BarangState> {
     }
   }
 
-  Future<void> _onAddBarang(AddBarang event, Emitter<BarangState> emit) async {
+  Future<void> onAddBarang(AddBarang event, Emitter<BarangState> emit) async {
     try {
       await barangRepository.insertBarang(event.barang);
       emit(BarangOperationSuccess('Barang added successfully'));
@@ -38,7 +38,7 @@ class BarangBloc extends Bloc<BarangEvent, BarangState> {
     }
   }
 
-  Future<void> _onUpdateBarang(UpdateBarang event, Emitter<BarangState> emit) async {
+  Future<void> onUpdateBarang(UpdateBarang event, Emitter<BarangState> emit) async {
     try {
       await barangRepository.updateBarang(event.barang);
       emit(BarangOperationSuccess('Barang updated successfully'));
@@ -48,7 +48,7 @@ class BarangBloc extends Bloc<BarangEvent, BarangState> {
     }
   }
 
-  Future<void> _onDeleteBarang(DeleteBarang event, Emitter<BarangState> emit) async {
+  Future<void> onDeleteBarang(DeleteBarang event, Emitter<BarangState> emit) async {
     try {
       await barangRepository.deleteBarang(event.id);
       emit(BarangOperationSuccess('Barang deleted successfully'));
@@ -58,7 +58,7 @@ class BarangBloc extends Bloc<BarangEvent, BarangState> {
     }
   }
 
-  Future<void> _onDeleteMultipleBarang(DeleteMultipleBarang event, Emitter<BarangState> emit) async {
+  Future<void> onDeleteMultipleBarang(DeleteMultipleBarang event, Emitter<BarangState> emit) async {
     try {
       await barangRepository.deleteMultipleBarang(event.ids);
       emit(BarangOperationSuccess('Selected barang deleted successfully'));
